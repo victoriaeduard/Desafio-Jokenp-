98e3ddf3a47b81aff4466f6926e1e8ff43fa365f
Victória Eduarda <vitoriaeduarda853@gmail.com>
2026-01-10 01:56:03 +0000

    commit printf

--- supertrunfo.c
+++ supertrunfo.c
@@ -6,37 +6,18 @@ int main (){
 
 int jogador, pc, atributo;
 
-// população
-int pop_brasil = 214;
-int pop_eua = 331;
-int pop_japao = 125;
-
-// PIB
-float pib_brasil = 11.8;
-float pib_eua = 28.6;
-float pib_japao = 2.3;
-
-// área 
-float area_brasil = 8516.8;
-float area_eua = 9834.4;
-float area_japao = 378.5;
-
-//pontos turisticos
-int turis_brasil = 34;
-int turis_eua = 41;
-int turis_japao = 25;
-
-//densidade
-float densi_brasil = 28.3;
-float densi_eua = 38.2;
-float densi_japao = 338.6;
+int populacao [3] = {214, 331, 125};
+float pib [3] = {11.8, 28.6, 2.3};
+float area [3] = {8516.8, 9834.4, 378.5};
+int turistas [3] = {34, 41, 25};
+float densidade [3] = {28.3, 38.2, 338.6};
 
 printf("Bem-vindo ao Super Trunfo! Anter de começar, confira o funcionamento do jogo:");
 printf("O jogo consiste em escolher o atributo de uma carta para compará-la a outra. Ganha a carta com valor mais alto, com exceção do atributo Densidade.");
 printf("Primeiro escolha uma carta:\n");
-printf("1. Brasil\n");
-printf("2. Estados Unidos\n");
-printf("3. Japão\n");
+printf("1. Carta 01: Brasil\n");
+printf("2. Carta 02: Estados Unidos\n");
+printf("3. Carta 03: Japão\n");
 scanf("%d", &jogador);
 printf("Agora escolha um Atributo para comparar:\n");
 printf("1. População\n");
@@ -46,19 +27,18 @@ printf("4. Pontos Turisticos\n");
 printf("5. Densidade Demográfica\n");
 scanf("%d", &atributo);
 
-
 srand (time(0));
 pc = rand() % 3 + 1;
 
 switch (jogador){
 case 1:
-printf("Jogador: Brasil - ");
+printf("Jogador: Carta 01 Brasil - ");
     break;
     case 2:
-printf("Jogador: Estados Unidos - ");
+printf("Jogador: Carta 02 Estados Unidos - ");
     break;
     case 3:
-printf("Jogador: Japão - ");
+printf("Jogador Carta 03 Japão - ");
     break;
 default:
 printf("Escolha invalida.");
@@ -66,20 +46,37 @@ printf("Escolha invalida.");
 
 switch (pc){
 case 1:
-printf("Pc: Brasil - \n");
+printf("Pc: Carta 01 Brasil - \n");
     break;
     case 2:
-printf("Pc: Estados Unidos - \n");
+printf("Pc: Carta 02 Estados Unidos - \n");
     break;
     case 3:
-printf("Pc: Japão - \n");
+printf("Pc: Carta 03 Japão - \n");
+    break;
+    default:
+printf("Escolha invalida.");
+}
+
+switch (atributo){
+    case 1:
+printf("Atributo: População\n");
+    break;
+    case 2:
+printf("Atributo: Área\n");
+    break;
+    case 3:
+printf("Atributo: PIB\n");
+    break;
+    case 4:
+printf("Atributo: Pontos Turísticos\n");
+    break;
+    case 5:
+printf("Atributo: Densidade Demográfica\n");
     break;
 default:
 printf("Escolha invalida.");
 }
-
-printf("Atributo: %d", jogador);
-
 
 
 return 0;
