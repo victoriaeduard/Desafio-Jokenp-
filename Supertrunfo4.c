3ade882833a28db94e2a1d481a266d881586cbec
Victória Eduarda <vitoriaeduarda853@gmail.com>
2026-01-11 16:30:00 -0300

    Create Supertrunfo4.c

---
+++ Supertrunfo4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (){
+
+//variaveis
+int jogador, pc, atributo;
+//variaveis de acordo com a escolha, valores 0,1 e 2
+float populacao [3] = {220.05, 341.96, 123.20};
+float pib [3] = {11.8, 28.6, 2.3};
+float area [3] = {8516.8, 9834.4, 378.5};
+int turistas [3] = {34, 41, 25};
+float densidade [3] = {28.3, 38.2, 338.6};
