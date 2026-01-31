54c3cab354fe59f0bb4a25982c8b08811b2889bd
Victória Eduarda <vitoriaeduarda853@gmail.com>
2026-01-31 12:59:15 +0000

    ternário

--- Supertrunfo4.c
+++ Supertrunfo4.c
@@ -27,7 +27,6 @@ printf("1. Carta 01B\n");
 printf("2. Carta 02E\n");
 printf("3. Carta 03J\n");
 scanf("%d", &jogador);
-printf("\n\n");
 
 //gerador de numero aleatório
 srand (time(0));
@@ -99,10 +98,8 @@ scanf("%d", &atributo2);
 break;
 }
 printf("\n\n");
-
 if (atributo == atributo2){ printf("Atributos iguais. Jogo encerrado!"); 
-}
-return 0;
+return 0; }
 
 //apresentação da escolha da carta do jogador e computador
 switch (jogador){
@@ -135,8 +132,7 @@ printf("Escolha invalida.");
 
 if (idjogador == idpc){
     printf("Cartas iguais. Empate!");
-}
-return 0;
+return 0; }
 
 printf("\n\n");
 //exibição do atributo escolhido
@@ -144,19 +140,24 @@ printf("--Hora da batalha!--\n");
 switch (atributo){
     case 1:
 printf("Atributo: População\nJogador: %f - Computador: %f\n", populacao [idjogador], populacao [idpc]);
-    break;
+populacao [idjogador] > populacao [idpc] ? printf("###Rodada ganha!###\n") : printf("###Rodada perdida!###\n");
+break;
     case 2:
-printf("Atributo: Área\nJogador: %f - Computador: %f", area [idjogador], area [idpc]);
-    break;
+printf("Atributo: Área\nJogador: %f - Computador: %f\n", area [idjogador], area [idpc]);
+area [idjogador] > area [idpc] ? printf("###Rodada ganha!###\n") : printf("###Rodada perdida!###\n");
+break;
     case 3:
-printf("Atributo: PIB\nJogador: %f - Computador: %f", pib [idjogador], pib [idpc]);
+printf("Atributo: PIB\nJogador: %f - Computador: %f\n", pib [idjogador], pib [idpc]);
+pib [idjogador] > pib [idpc] ? printf("###Rodada ganha!###\n") : printf("###Rodada perdida!###\n");
     break;
     case 4:
-printf("Atributo: Pontos Turísticos\nJogador: %d - Computador: %d", turistas [idjogador], turistas [idpc]);
-    break;
+printf("Atributo: Pontos Turísticos\nJogador: %d - Computador: %d\n", turistas [idjogador], turistas [idpc]);
+turistas [idjogador] > turistas [idpc] ? printf("###Rodada ganha!###\n") : printf("###Rodada perdida!###\n");
+break;
     case 5:
 printf("Atributo: Densidade Demográfica\nJogador: %f - Computador: %f\n", densidade [idjogador], densidade [idpc]);
-    break;
+densidade [idjogador] < densidade [idpc] ? printf("###Rodada ganha!###\n") : printf("###Rodada perdida!###\n");
+break;
 default:
 printf("Escolha invalida.");
 }
@@ -165,63 +166,27 @@ printf("\n");
 switch (atributo2){
     case 1:
 printf("Segundo Atributo: População\nJogador: %f - Computador: %f\n", populacao [idjogador], populacao [idpc]);
-    break;
+populacao [idjogador] > populacao [idpc] ? printf("###Rodada ganha!###\n") : printf("###Rodada perdida!###\n");
+break;
     case 2:
-printf("Segundo Atributo: Área\nJogador: %f - Computador: %f", area [idjogador], area [idpc]);
-    break;
+printf("Segundo Atributo: Área\nJogador: %f - Computador: %f\n", area [idjogador], area [idpc]);
+area [idjogador] > area [idpc] ? printf("###Rodada ganha!###\n") : printf("###Rodada perdida!###\n");
+break;
     case 3:
-printf("Segundo Atributo: PIB\nJogador: %f - Computador: %f", pib [idjogador], pib [idpc]);
-    break;
+printf("Segundo Atributo: PIB\nJogador: %f - Computador: %f\n", pib [idjogador], pib [idpc]);
+pib [idjogador] > pib [idpc] ? printf("###Rodada ganha!###\n") : printf("###Rodada perdida!###\n");
+break;
     case 4:
-printf("Segundo Atributo: Pontos Turísticos\nJogador: %d - Computador: %d", turistas [idjogador], turistas [idpc]);
-    break;
+printf("Segundo Atributo: Pontos Turísticos\nJogador: %d - Computador: %d\n", turistas [idjogador], turistas [idpc]);
+turistas [idjogador] > turistas [idpc] ? printf("###Rodada ganha!###\n") : printf("###Rodada perdida!###\n");
+break;
     case 5:
 printf("Segundo Atributo: Densidade Demográfica\nJogador: %f - Computador: %f\n", densidade [idjogador], densidade [idpc]);
-    break;
+densidade [idjogador] < densidade [idpc] ? printf("###Rodada ganha!###\n") : printf("###Rodada perdida!###\n");
+break;
 default:
 printf("Escolha invalida.");
 }
 printf("\n");
 printf("###Atributo Especial SUPER TRUNFO###\nJogador: %f - Computador: %f", trunfo [idjogador], trunfo [idpc]);
-
-printf("\n\n");
-//resultado definitivo
-printf("--Resultado final--\n");
-
-if (atributo == 1 (populacao [idjogador] > populacao [idpc])){ printf("###Primeiro atributo: Você venceu!###\n");
-} else if{ printf("###Você perdeu!###\n"); }
-
-if (atributo == 2){
-area [idjogador] > area [idpc] ? printf("###Primeiro atributo: Você venceu!###\n") : printf("###Primeiro atributo: Você perdeu!###\n"); }
-
-if (atributo == 3){
-pib [idjogador] > pib [idpc] ? printf("###Primeiro atributo: Você venceu!###\n") : printf("###Primeiro atributo: Você perdeu!###\n"); }
-
-if (atributo == 4){
-turistas [idjogador] > turistas [idpc] ? printf("###Primeiro atributo: Você venceu!###\n") : printf("###Primeiro atributo: Você perdeu!###\n"); }
-
-if (atributo == 5){
-densidade [idjogador] < densidade [idpc] ? printf("###Primeiro atributo: Você venceu!###\n") : printf("###Primeiro atributo: Você perdeu!###\n"); }
-
-if (atributo == atributo2){ printf ("Atributo repetido. Jogo encerrado.");
-}
-return 0;
-
-if (atributo2 == 1){
-populacao [idjogador] > populacao [idpc] ? printf("###Você venceu!###\n") : printf("###Segundo Atributo: Você perdeu!###");
-}
-
-if (atributo2 == 2){
-area [idjogador] > area [idpc] ? printf("###Segundo atributo: Você venceu!###\n") : printf("###Segundo atributo: Você perdeu!###\n"); }
-
-if (atributo2 == 3){
-pib [idjogador] > pib [idpc] ? printf("###Segundo atributo: Você venceu!###\n") : printf("###Segundo atributo: Você perdeu!###\n"); }
-
-if (atributo2 == 4){
-turistas [idjogador] > turistas [idpc] ? printf("###Segundo atributo: Você venceu!###\n") : printf("###Segundo atributo: Você perdeu!###\n"); }
-
-if (atributo2 == 5){
-densidade [idjogador] < densidade [idpc] ? printf("###Segundo atributo: Você venceu!###\n") : printf("###Segundo atributo: Você perdeu!###\n"); }
-
-return 0;
 }
