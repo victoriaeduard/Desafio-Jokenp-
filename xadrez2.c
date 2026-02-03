746fbc46f4b089122133f05243691ad7a1527da4
Victória Eduarda <vitoriaeduarda853@gmail.com>
2026-02-03 18:42:56 +0000

    finalização com a Rainha

--- xadrez2.c
+++ xadrez2.c
@@ -3,12 +3,12 @@
 int main (){
 
     //introdução das variaveis
-    int i = 0, i2 = 0;
+    int i = 1, i2 = 1;
 
 printf("Simulação do movimento de peças de xadrez com a Torre:\n");
 //execução do movimento da Torre com while
 while (i <=5 ){
-printf("A Torre se moveu %d casa(s) para direita\n", i);
+printf("A Torre se moveu %d casa(s) horizontalmente para direita\n", i);
 i++;
 }
 
@@ -16,12 +16,18 @@ printf("\n");
 printf("Simulação do movimento de peças de xadrez com o Bispo:\n");
 //execução do movimento do Bispo com do-while
 do {
-    printf("%d", i2);
     if (i2 <= 5){
-        printf("O bispo se moveu %d para direita\n", i2);
-        i2++
+        printf("O Bispo se moveu %d casa(s) para cima, direita\n", i2);
+        i2++;
     }
 } while (i2 <= 5);
+
+printf("\n");
+printf("Simulação do movimento de peças de xadrez com a Rainha:\n");
+//execução do movimento da Torre com for
+for (int i3 = 1; i3 <= 8; i3++){
+    printf("A Rainha se moveu %d casa(s) para esquerda\n", i3);
+}
 
 return 0;
 }
