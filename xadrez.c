0990ea47910be7c33d2aa310c4dce8c3cf007545
Victória Eduarda <vitoriaeduarda853@gmail.com>
2026-02-17 14:53:31 +0000

    peça do cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int movimentobispo;
+int movimentobispo = 8, movimentocavalo = 1;
 int i;
 
 //função recursiva da torre
@@ -11,9 +11,9 @@ void moverTorre(int casas, int direcao) {
 
     //movimentação da torre
     if (direcao == 1) {
-        printf("Torre moveu 1 casa para a Direita\n");
+        printf("Direita\n");
     } else if (direcao == 2) {
-        printf("Torre moveu 1 casa para a Esquerda\n");
+        printf("Esquerda\n");
     }
 
     // chamada recursiva
@@ -24,16 +24,24 @@ void moverBispo(int casas, int direcao) {
     if (casas <= 0) {
         return;
     }
-
-    //movimentação do bispo
+    // movimentação do bispo
     for (int i = 0; i < 1; i++) {
         for (int j = 0; j < 1; j++) {
-            if (direcao == 1) printf("O Bispo se moveu para Cima, Direita.\n");
-            else printf("O Bispo se moveu para Cima, Esquerda");
+
+
+            if (direcao == 1) {
+                printf("Cima\n");
+                printf("Direita\n");
+            } 
+            else if (direcao == 2) {
+                printf("Cima\n");
+                printf("Esquerda\n");
+            }
         }
     }
     moverBispo(casas - 1, direcao);
 }
+
 
 //função recursiva da rainha
 void moverRainha(int casas, int direcao) {
@@ -43,20 +51,51 @@ void moverRainha(int casas, int direcao) {
 
     //movimentação da rainha
     if (direcao == 1) {
-        printf("Rainha moveu 1 casa para a Direita\n");
+        printf("Direita\n");
     } else if (direcao == 2) {
-        printf("Bispo moveu 1 casa para a Esquerda\n");
+        printf("Esquerda\n");
     }
 
     // chamada recursiva
     moverRainha(casas - 1, direcao);
 }
 
+//movimento do cavalo
+void moverCavalo(int casas, int direcao) {
+    if (casas == 0) {
+        return;
+    }
+    for (int vertical = 0; vertical < 2; vertical++) {
+        //movimento vertical
+        if (direcao == 1 || direcao == 2) {
+            printf("Cima\n");
+        } 
+        else if (direcao == 3 || direcao == 4) {
+            printf("Baixo\n");
+        }
+
+        //movimento horizontal (só após a segunda casa vertical)
+        for (int horizontal = 0; horizontal < 1; horizontal++) {
+            if (vertical == 1) {
+
+                if (direcao == 1 || direcao == 3) {
+                    printf("Direita\n");
+                } 
+                else if (direcao == 2 || direcao == 4) {
+                    printf("Esquerda\n");
+                }
+            }
+        }
+    }
+    moverCavalo(casas - 1, direcao);
+}
+
+
 int main() {
 
     int peca;
     int direcao;
-    int movimentorre,  movimentobispo, movimentorainha;
+    int movimentorre, movimentorainha;
 
     printf("--------------------------------------------------\n");
     printf("Simulação de peças de xadrez. Escolha uma peça:\n");
@@ -81,6 +120,7 @@ int main() {
             scanf("%d", &movimentorre);
 
             moverTorre(movimentorre, direcao);
+            break;
             case 2:
             printf("Voce escolheu o Bispo (ps: ele move-se obrigatoriamente 8 casas).\n");
             printf("Escolha a direcao:\n");
@@ -102,7 +142,19 @@ int main() {
             printf("Digite o numero de casas:\n");
             scanf("%d", &movimentorainha);
 
-            moverBispo(movimentorainha, direcao);
+            moverRainha(movimentorainha, direcao);
+            break;
+        case 4:
+            printf("Voce escolheu o Cavalo (ps: ele move-se obrigatoriamente uma vez.)\n");
+            printf("Escolha a direcao:\n");
+            printf("1. Duas cima, direita\n");
+            printf("2. Duas cima, esquerda\n");
+            printf("3. Duas baixo, direita\n");
+            printf("4. Duas baixo, esquerda\n");
+
+            scanf("%d", &direcao);
+
+            moverCavalo(movimentocavalo, direcao);
             break;
         default:
             printf("Opção invalida.\n");
